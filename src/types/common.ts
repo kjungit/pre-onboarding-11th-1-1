eff46756e2697f8dc7d05516a6e514f37c501a7b
who0803 <10rlatkdtn@naver.com>
2023-06-29 18:48:02 +0900

    Feat: Inplement types about Auth
    signin, signup 관련 타입 지정

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,1 +1,38 @@
-export {};
+import { ReactNode } from 'react';
+
+export interface AuthInputValue {
+  // 사용
+  email: string;
+  password: string;
+}
+
+export interface FormErrors {
+  emailError: string;
+  passwordError: string;
+}
+
+export interface validateInputProps {
+  authInput: AuthInputValue;
+  validate: (values: AuthInputValue) => FormErrors;
+  setFormErrors: React.Dispatch<React.SetStateAction<FormErrors>>;
+  setIsDisabled: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export interface Token {
+  access_token: string;
+}
+
+export interface AuthFormProps {
+  isSignUp: boolean;
+  isDisabled: boolean;
+  authHandler: (event: React.FormEvent) => void;
+  children: ReactNode;
+}
+
+export interface AuthInputProps {
+  type: string;
+  isAutoComplete: 'on' | 'off';
+  error: string;
+  isFocus: boolean;
+  inputChangeHandler: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
