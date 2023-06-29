e06f07da8039e9cfc3288d66d5ae5614b079c0fb
khkh0109 <ist200120@gmail.com>
2023-06-29 17:03:55 +0900

    Feat : Localstorage get, set, remove 함수 추가

--- src/utils/Localstorage.ts
+++ src/utils/Localstorage.ts
@@ -1,1 +1,17 @@
-export {};
+interface Response {
+  access_token: string;
+}
+
+export const getAccessToken = (key: string): string | null => {
+  const accessToken = localStorage.getItem(key);
+  return accessToken;
+};
+
+export const setAccessToken = (response: Response) => {
+  const { access_token } = response;
+  localStorage.setItem('accessToken', access_token);
+};
+
+export const removeAccessToken = (key: string) => {
+  localStorage.removeItem(key);
+};
