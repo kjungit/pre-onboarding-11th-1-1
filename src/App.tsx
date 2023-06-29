2da43324261cf21da524bee5d74be63e282b6227
Sumin Sohn <soyw31@gmail.com>
2023-06-29 18:07:06 +0900

    Feat: 투두 추가 컴포넌트 작성 (AddTodo)
    - 로그인을 해야 투두 api를 호출할 수 있기 때문에 임시로 로그인 시켜놨음

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import { useEffect } from 'react';
+import TodoPage from './pages/TodoPage';
+import { AuthApi } from './apis/lib/auth';
+import { TOKEN } from './utils/constants';
 
 function App() {
-  return <div className='App'>TODO</div>;
+  // 임시 코드 (로그인 해야 투두리스트 불러올 수 있기 때문)
+  useEffect(() => {
+    if (localStorage.getItem(TOKEN)) return;
+
+    AuthApi.signIn('dkssudgktpdy1@', 'dkssudgktpdy1!').then(
+      ({ access_token }) => {
+        localStorage.setItem(TOKEN, access_token);
+      },
+    );
+  }, []);
+
+  return <TodoPage />;
 }
 
 export default App;
