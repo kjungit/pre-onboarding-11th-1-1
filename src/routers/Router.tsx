fe0e22373d6dbf5b25bb9b7ecd98c834b23b9015
kjun <kjun_all@naver.com>
2023-06-30 03:15:00 +0900

    Feat: createBrowserRouter 설정

--- src/routers/Router.tsx
+++ src/routers/Router.tsx
@@ -1,1 +1,32 @@
-export {};
+import { createBrowserRouter } from 'react-router-dom';
+import JoinPage from '../pages/JoinPage';
+import LoginPage from '../pages/LoginPage';
+import TodoPage from '../pages/TodoPage';
+import NotFoundPage from '../pages/NotFoundPage';
+import { redirectLogin, redirectTodo } from './redirect';
+import { Root } from './Root';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        path: '/join',
+        element: <JoinPage />,
+        loader: redirectTodo,
+      },
+      {
+        path: '/login',
+        element: <LoginPage />,
+        loader: redirectTodo,
+      },
+      {
+        path: '/todo',
+        element: <TodoPage />,
+        loader: redirectLogin,
+      },
+    ],
+    errorElement: <NotFoundPage />,
+  },
+]);
