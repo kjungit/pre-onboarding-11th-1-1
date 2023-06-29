50fdb733d679c220ea5d9d58462b396447800b57
hihijin <117073214+hihijin@users.noreply.github.com>
2023-06-29 17:40:25 +0900

    Feat : loading,error페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App() {
-  return <div className='App'>TODO</div>;
+  return (
+    <div className='flex h-screen w-full flex-col items-center justify-center text-gray-800'>
+      <div className='mb-10 text-4xl font-bold'>404 Not Found</div>
+    </div>
+  );
 }
 
 export default App;
