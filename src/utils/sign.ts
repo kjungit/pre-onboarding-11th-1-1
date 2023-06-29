81603fe66fc28a11f4e77f65f7bedd56ad63251c
who0803 <10rlatkdtn@naver.com>
2023-06-29 19:01:41 +0900

    Feat: Implement validate utils
    - isValidEmail : 이메일 유효성 검사 함수
    - validateInput : validate 함수를 매개변수로 받아서 error message,
      button disabled를 설정하는 함수
      	- 매개변수
    		- authInput : form 관련 인풋 값
    		- validate : 사용자 지정 유효성 검사 함수로써
    		  에러메세지를 리턴
    		- setFormErrors: error 상태 변경 함수
    		- setIsDisabled: isDisabled 상태 변경 함수

---
+++ src/utils/sign.ts
@@ -0,0 +1,25 @@
+import { validateInputProps } from '../types/common';
+
+export const isValidEmail = (email: string) => {
+  const emailPattern = /@/;
+  return emailPattern.test(email);
+};
+
+export const validateInput = ({
+  authInput,
+  validate,
+  setFormErrors,
+  setIsDisabled,
+}: validateInputProps) => {
+  const error = validate(authInput);
+
+  const isValid = () => {
+    return (
+      Object.values(error).every((err) => err === '') &&
+      Object.values(authInput).every((val) => val !== '')
+    );
+  };
+
+  setFormErrors(error);
+  setIsDisabled(!isValid());
+};
