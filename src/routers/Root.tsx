7eaed0b6b9cb0437d83e525e79d1acc4a7ec2547
kjun <kjun_all@naver.com>
2023-06-30 17:47:40 +0900

    Feat: 로그인 여부에 따른 헤더 버튼 조건부 렌더링 수정

--- src/routers/Root.tsx
+++ src/routers/Root.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
+import { getAccessToken } from '../utils/localStorage';
 
 export function Root() {
+  const location = useLocation();
+  const isLogined = getAccessToken() !== null;
+
   return (
     <>
-      <Header />
+      <Header isLogined={isLogined} />
       <Outlet />
       <Footer />
     </>
