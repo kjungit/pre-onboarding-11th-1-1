e6d8db62c1136299194eb45ffbd7ae1b52a2e514
kjun <kjun_all@naver.com>
2023-06-30 15:53:35 +0900

    Feat: 토큰 값에 따른 조건부렌더링

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,4 +1,13 @@
+import { Link, redirect } from 'react-router-dom';
+
 function Header() {
+  const token = localStorage.getItem('accessToken');
+
+  const onClickHandler = () => {
+    // 로그아웃 처리 API
+    redirect('/login');
+  };
+
   return (
     <header className='bg-black'>
       <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
@@ -6,24 +15,31 @@ function Header() {
           <h1 className='cursor-pointer text-sm font-bold text-white sm:text-xl'>
             wanted-pre-onboarding-frontend
           </h1>
-          {/* { ? (
+          {token !== null ? (
             <div>
               <button
-                className="items-center justify-center p-2 font-bold text-gray-200 rounded-md nline-flex hover:bg-gray-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                onClick={onClickHandler}
+                className='nline-flex items-center justify-center rounded-md p-2 font-bold text-gray-200 hover:bg-gray-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'
               >
                 로그아웃
               </button>
             </div>
-          ) : ( */}
-          <div className='flex text-sm sm:text-[16px] '>
-            <button className='nline-flex items-center justify-center rounded-md p-2 text-gray-200 hover:bg-gray-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
-              로그인
-            </button>
-            <button className='nline-flex items-center justify-center rounded-md p-2 text-gray-200 hover:bg-gray-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
-              회원가입
-            </button>
-          </div>
-          {/* )} */}
+          ) : (
+            <div className='flex text-sm sm:text-[16px] '>
+              <Link
+                to='/login'
+                className='nline-flex items-center justify-center rounded-md p-2 text-gray-200 hover:bg-gray-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'
+              >
+                로그인
+              </Link>
+              <Link
+                to='/join'
+                className='nline-flex items-center justify-center rounded-md p-2 text-gray-200 hover:bg-gray-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'
+              >
+                회원가입
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </header>
